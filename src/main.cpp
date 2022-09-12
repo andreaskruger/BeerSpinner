19870aa37595994cb182de1d97134c73834a063d
Andreas Axén <andreas.axen.kruger@gmail.com>
2022-09-12 20:03:17 +0200

    INit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <TB6612FNG.h>
+
+
+#define AIN1 25
+#define AIN2 26
+#define PWMA 33
+#define STBY 32
+#define potPin 36
+#define ADC_MAX 4096
+
+int potValue = 0;
+int potMap = 0;
+
+Tb6612fng motor(STBY, AIN1, AIN2, PWMA);
+
+void setup() {
+  Serial.begin(115200);
+  motor.begin();
+  pinMode(potPin,INPUT);
+}
+
+void loop() {
+  int time = millis();
+  potValue = analogRead(potPin);
+  potMap = map(potValue, 0,ADC_MAX,0,255);
+  motor.drive(potMap, 100);
+
+  int currentTime = time - millis();
+  //Serial.print(currentTime);
+}

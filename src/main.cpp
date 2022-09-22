14440800f7dae2abb085f328cd8c1887f85852b4
Andreas Axén <andreas.axen.kruger@gmail.com>
2022-09-22 11:08:47 +0200

    Added main

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,31 @@
 #define AIN2 26
 #define PWMA 33
 #define STBY 32
-#define potPin 15
+#define potPin 36
 #define ADC_MAX 4096
 
 int potValue = 0;
-int potMap = 0;
+float potMap = 0;
 
 Tb6612fng motor(STBY, AIN1, AIN2, PWMA);
 
 void setup() {
   Serial.begin(115200);
   motor.begin();
-  WIFI_init();
+  //WIFI_init();
   pinMode(potPin,INPUT);
+  motor.drive(0,1000);
+  motor.drive(0.2,1000);
+  motor.drive(0,1000);
 }
 
 void loop() {
   int time = millis();
   potValue = analogRead(potPin);
-  potMap = map(potValue, 0,ADC_MAX,0,255);
-  motor.drive(potMap, 100);
+  potMap = map(potValue, 0,ADC_MAX,0,100);
+  potMap =potMap/100;
+  Serial.println(potMap);
+  motor.drive(potMap, 10);
   int currentTime = time - millis();
   //Serial.print(currentTime);
 }
